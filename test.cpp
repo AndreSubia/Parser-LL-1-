36fa70e06ebc52fce2e50ec2f1d5964a400b9e90
André Subia <regulus096@gmail.com>
2021-07-03 11:35:53 -0500

    23/06

--- test.cpp
+++ test.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <map>
 #include <vector>
+
 using namespace std;
+
+char FIN = '$';
+
+template<typename K, typename V>
+std::ostream &operator<<(ostream &out, const map<K, V> &m) {
+    for (const std::pair<K, V> &p: m) {
+        out << p.first << " -> ";
+        for(int i=0; i<p.second.size();i++){
+            out << p.second[i] << " ";
+        }
+        out<<"\n";
+    }
+    return out;
+}
 
 class Produccion{
     public:
         string izquierda;
-        string derecha;
+        vector<char> produccion;
     public:
         Produccion(){};
+        friend ostream& operator <<(ostream &out, Produccion const& p){
+            for(int i = 0; i < p.produccion.size(); i++){
+                if(i=1){out<<" ->";}
+                else{
+                    out<<" "<<p.produccion[i];
+                }
+            }
+            return out;
+        }
 };
 
 /* struct test{
@@ -21,16 +47,38 @@ class Gramatica{
         //vector<string> noTerminales;
         vector<Produccion> producciones;
     public:
-        Gramatica(){};
+        Gramatica(string gramatica_txt){
+            ifstream archivo;
+            char letra;
+            Produccion p;
+            archivo.open(gramatica_txt);
+            if(archivo.fail()){return;}
+            while(!archivo.eof()){
+                archivo.get(letra);
+                if(letra != ' '){
+                    cout<<letra<<endl;
+                    p.produccion.push_back(letra);
+                    if(letra == '\n'){
+                        producciones.push_back(p);
+                        Produccion p;
+                    }
+                }
+            }
+            for(int i=0;i<producciones.size();i++){
+                cout<<producciones[i]<<endl;
+            }
+
+        };
         /* bool esTerminal(char c){
             if((c>=65 && c<=90)||(c>=97 && c<=122)){
                 return false;
             }
             return true;
         } */
-        vector<Produccion> getProducciones(string nt);
+        //vector<Produccion> getProducciones(string nt);
 };
 
 int main(){
+    Gramatica ll1("gramatica.txt");
     return 0;
 }
